215ee89cac174c931f2f3a4cfd02cd23d3b1be0e
Nils Neuhäusel <n.neuhaeusel@gmail.com>
2022-02-27 16:20:38 +0100

    Implemented whole project :)

--- app/src/main/java/com/example/statecompose/MainActivity.kt
+++ app/src/main/java/com/example/statecompose/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.statecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.statecompose.ui.theme.StateComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +32,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column
+    var nameState by remember {
+        mutableStateOf("")
+    }
+    var name by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text="Hello $name")
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(value = nameState, onValueChange = {
+            nameState = it
+        })
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            name = nameState
+        }) {
+            Text(text = "Display")
+        }
+    }
 }
 
 @Preview(showBackground = true)
